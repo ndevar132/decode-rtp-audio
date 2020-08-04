3f1871fe431c375413026db0abc791fcb4897798
ndevar132 <l4novan132@gmail.com>
2020-08-05 06:00:14 +0700

    checked for pair of rtp communication, to be considered valid

--- rtp/decode.py
+++ rtp/decode.py
@@ -1,6 +1,7 @@
 import dpkt
 import pcap
 import os
+import socket
 import time
 import argparse
 from collections import defaultdict
@@ -26,15 +27,30 @@ def decode_audio(pc):
                 if sport < 1024 or dport < 1024 or udp_payload[0] != 128:
                     continue
 
-                print(udp_payload, '\n')
+                # print(udp_payload, '\n')
     
                 # rtp payload starts at bytes 13 (offset 12 bytes for rtp header)
                 rtp_payload = udp_payload[12:]
                 payload_dict[(src, dst, sport, dport)].append(rtp_payload)
                 number_rtppackets += 1
 
-                if number_rtppackets >= 10:
+                if number_rtppackets >= 1000:
                     break
+
+    for [src, dst, sport, dport], payload_list in payload_dict.items():
+        all_payloads = b''.join(payload_list)
+        src_str = socket.inet_ntoa(src)
+        dst_str = socket.inet_ntoa(dst)
+
+        reverse_src, reverse_dst, reverse_sport, reverse_dport = dst, src, dport, sport
+        
+        # if there is a pair of rtp communication, consider valid audio
+        number_singlertp = 0
+        if (reverse_src, reverse_dst, reverse_sport, reverse_dport) not in payload_dict:
+            number_singlertp += 1
+            continue
+
+    print("There are {} single rtp in {} packets".format(number_singlertp, number_rtppackets))
 
                 
     
