fb149e3f79719911339c810692825f10d4c9a302
ndevar132 <l4novan132@gmail.com>
2020-08-05 06:11:34 +0700

    parsed argument from command line for pcap file name

--- split_audio.py
+++ split_audio.py
@@ -1,8 +1,15 @@
 from rtp.decode import decode_audio
 import dpkt
+import argparse
 
 if __name__ == '__main__':
-    pcap_file = 'test_180s.pcap'
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('-f', '--file', required=True, help="pcap file")
+    
+    args = vars(ap.parse_args())
+
+    pcap_file = args['file']
     pc = dpkt.pcap.Reader(open(pcap_file, 'rb'))
     trafficfilter = 'rtp'
     pc.setfilter(trafficfilter)
